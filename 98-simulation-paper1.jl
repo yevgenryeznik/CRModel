b49b76f975783b377fb4ee57552ec26511ed0461
Yevgen Ryeznik <yevgenryeznik@users.noreply.github.com>
2025-04-14 18:42:14 +0200

    update has been added to 98-simulation-paper1.jl

--- 98-simulation-paper1.jl
+++ 98-simulation-paper1.jl
@@ -11,6 +11,7 @@ using LaTeXStrings
 using LinearAlgebra
 using Match
 using NonlinearSolve
+using Pipe
 using Printf
 using Random: seed!
 using Zygote
@@ -109,5 +110,97 @@ fig, ax = check_optimality(
 xlims!(ax, xlimits[d][1], xlimits[d][2]+0.5)
 ylims!(ax, ylimits[d])
 fig 
+
+
+# simulating design for estimating operating characteristics
+function fixed_optimal_design(ξ::Design, nsbj::Int64, θ::Vector{<:Number}, Lθ::Vector{<:Number}, Uθ::Vector{<:Number}; N = 50, max_iter = 1000, seed = 314159)    
+    # simulating one adaptive design
+    seed!(seed);
+    
+    # generating observations
+    obs = generate_obs(nsbj, ξ, θ)
+
+    # defining negative log-likelihood function 
+    f(θ) = nll(θ, obs)
+
+    # finding MLEs by PSO
+    swarm = Swarm(N, max_iter, Lθ, Uθ, (0.9, 0.4), 1.5, 2.5, 0.5, 1e-12, f);
+    θmle = optimize_mle!(f, swarm)
+
+    return θmle, obs
+end
+
+# sample size
+nsbj = 40
+
+# number of simulations
+nsim = 1000
+
+ϵ = 1e-10
+b = 20
+Lθ = [-b, ϵ, -b, ϵ]
+Uθ = [ b, b, -ϵ, b]
+
+mle_θ = Vector[]
+obs_ξ = Vector[]
+
+
+for d in eachindex(ξ)
+    push!(mle_θ, Vector[])
+    push!(obs_ξ, Vector[])
+
+    @time for s in 1:nsim
+        mle_, obs_ = fixed_optimal_design(ξ[d], nsbj, θ, Lθ, Uθ, seed = 314159+2*s);
+        push!(mle_θ[d], mle_)
+        push!(obs_ξ[d], obs_)
+
+        println("simulation $s is complete!")
+    end    
+end
+
+
+
+function summarize(mle_θ, design)
+    OBDhat = [obd(item)[1] for item in mle_θ]
+    MTDhat = [mtd(item)[1] for item in mle_θ]
+
+    keep = (0 .< MTDhat .< 10) .& (0 .< OBDhat .< 10)
+
+    OBD = obd(θ)[1]    
+    OBDbias = OBD - mean(OBDhat[keep])
+    OBDsd = std(OBDhat[keep])
+    OBDrmse = sqrt(OBDbias^2 + OBDsd^2)
+
+    MTD = mtd(θ)[1]
+    MTDbias = MTD - mean(MTDhat[keep])
+    MTDsd = std(MTDhat[keep])
+    MTDrmse = sqrt(MTDbias^2 + MTDsd^2)
+
+    Pr_NoMLE = round(1-mean(keep), digits = 4)
+
+    @pipe DataFrame(
+        "MTD-Bias" => round(MTDbias, digits = 4),
+        "MTD-SD"   => round(MTDsd, digits = 4),
+        "MTD-RMSE" => round(MTDrmse, digits = 4),
+        "OBD-Bias" => round(OBDbias, digits = 4),
+        "OBD-SD"   => round(OBDsd, digits = 4), 
+        "OBD-RMSE" => round(OBDrmse, digits = 4),
+        "Pr(No MLE)" => Pr_NoMLE
+    ) |>
+    stack(_, 
+        names(_), 
+        variable_name = "characteristic", 
+        value_name = design
+    )
+end
+
+# operating characteristics of the 4 designs
+innerjoin(
+    summarize(mle_θ[1], "I"),
+    summarize(mle_θ[2], "II"),
+    summarize(mle_θ[3], "III"),
+    summarize(mle_θ[4], "IV"),
+    on = :characteristic
+) 
 
 
